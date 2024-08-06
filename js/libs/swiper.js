811fb538832ad250d6916b61312d614d15bcabe4
charlesalmeiida <charlesalmeida844@gmail.com>
2024-08-06 18:48:36 -0300

    feat: create new sections

---
+++ js/libs/swiper.js
@@ -0,0 +1,17 @@
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 2.1,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+})
+
+var swiper = new Swiper(".contents", {
+  slidesPerView: 4.3,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+})
