9950a67ff8d4d9d83f4853671bb122bbd36a102a
charlesalmeiida <charlesalmeida844@gmail.com>
2024-08-06 10:26:28 -0300

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+const buttonFaq = document.querySelectorAll(".js-button-faq")
+
+buttonFaq.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    btn.parentElement.classList.toggle("active")
+  })
+})
