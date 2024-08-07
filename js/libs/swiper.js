5c73de5a176ce63b087d3512dd1a8a5bf53bd885
charlesalmeiida <charlesalmeida844@gmail.com>
2024-08-07 18:02:10 -0300

    feat: adjust responviness

--- js/libs/swiper.js
+++ js/libs/swiper.js
@@ -9,6 +9,20 @@ var swiper = new Swiper(".mySwiper", {
     nextEl: ".btn-next",
     prevEl: ".btn-prev",
   },
+  breakpoints: {
+    320: {
+      slidesPerView: 1.4,
+      spaceBetween: 24,
+    },
+    600: {
+      slidesPerView: 2.5,
+      spaceBetween: 16,
+    },
+    1200: {
+      slidesPerView: 3.1,
+      spaceBetween: 30,
+    },
+  },
 })
 
 var swiper = new Swiper(".contents", {
@@ -17,5 +31,19 @@ var swiper = new Swiper(".contents", {
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 1.4,
+      spaceBetween: 24,
+    },
+    600: {
+      slidesPerView: 2.5,
+      spaceBetween: 16,
+    },
+    1200: {
+      slidesPerView: 4.3,
+      spaceBetween: 32,
+    },
   },
 })
